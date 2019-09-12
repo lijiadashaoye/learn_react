e63ba429813e3022fc6b82cc043a010dcb1dcb7d
李朋 <15646507519@sian.cn>
2019-09-12 20:39:53 +0800

    f

--- src/components/one/one.jsx
+++ src/components/one/one.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './one.css';
 
+// 在 webpack.config.js 文件中定义好了路径别名： alias 属性下
 import Two from 'Src/components/two/two';
 import Three from 'Src/components/three/three';
 
@@ -14,15 +15,15 @@ export default class One extends React.Component {
         console.log(event.target)
         console.log(this)
         console.log(this.fff)
-
+        this.setState(() => this.fff = '改变了的数据 234234')
     }
 
     render() {
         return (<div className="one">
-            {this.fff}
-            {/* 在 webpack.config.js 文件中定义好了路径别名： alias 属性下 */}
-            <img alt='' style={{ width: '100px' }} src={require('Imgs/1.png')}></img>
-            <button onClick={this.clickFn}>clickFn</button>
+
+            <img alt='' style={{ width: '100px' }} src={require('Imgs/1.png')}></img><br />
+            <span style={{ color: 'blue', 'font-weight': 'bold' }}>{this.fff}</span>&nbsp;&nbsp;
+            <button onClick={this.clickFn}>绑定事件，改变数据</button>
             <p className='classd'>使用class样式</p>
             <p style={{ color: 'red' }}>使用class样式</p>
             <label htmlFor="labeld">label写法：</label>

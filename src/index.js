711babe5a3cfcd5782c8a5b9e4ea2d8ab256f5d9
李朋 <15646507519@sian.cn>
2019-09-16 20:16:49 +0800

    r

--- src/index.js
+++ src/index.js
@@ -8,26 +8,15 @@ import One from './components/one/one';
 import Two from './components/two/two';
 import Three from './components/three/three';
 
-const routes = {
-    path: '/',
-    component: One,
-    childRoutes: [
-        { path: '/two', component: Two },
-        { path: '/three', component: Three },
-    ]
-}
-
 ReactDOM.render(
     <Router>
         <Link to='/one'>one</Link> <br />
         <Link to='/two'>two</Link> <br />
         <Link to='/three'>three</Link>
-        <button onClick={chage}>chage</button>
         <div>
             <Route path='/one' component={One} obj={{ name: '初始化的传入数据' }} string='string_props' arrary={[1, 2, 3]} />
             <Route path='/two' component={Two} />
             <Route path='/three' component={Three} />
-
         </div>
     </Router>,
     document.querySelector('#root')
